bd4c804735b52b3bb6a9d9490fa0ead22af95d4f
ud <mizunoawa.web@gmail.com>
2024-06-08 17:53:05 +0900

    スクリプトファイルを分離

---
+++ FTGOverlayControl/FTGOverlayControl/template_maryucup6th/overlay.js
@@ -0,0 +1,82 @@
+const player1Elements = {
+    name: document.getElementById("playerName1"),
+    image: document.getElementById("player1"),
+    index: -1
+}
+const score1 = document.getElementById("score1");
+const player2Elements = {
+    name: document.getElementById("playerName2"),
+    image: document.getElementById("player2"),
+    index: -1
+}
+const score2 = document.getElementById("score2");
+
+
+// functions
+function createPlayerImage(fileName) {
+    var img = document.createElement("img");
+    img.setAttribute("src", fileName);
+    img.style.display = "none";
+    return img;
+}
+
+function switchPlayer(elements, index) {
+    if (elements.index == index) {
+        return;
+    }
+
+    if (elements.index >= 0) {
+        elements.image.children[elements.index].style.display = "none";
+    }
+    elements.image.children[index].style.display = "block";
+    elements.name.innerHTML = players[index].name;
+    elements.index = index;
+}
+
+function updateText() {
+    let obj;
+    readScore = async () => {
+        const response = await fetch('score.json');
+        const data = await response.text();
+        obj = JSON.parse(data);
+    }
+    readScore().then(() => {
+        const newP1Index = obj["player1"];
+        switchPlayer(player1Elements, newP1Index);
+        const newP2Index = obj["player2"];
+        switchPlayer(player2Elements, newP2Index);
+
+        score1.innerHTML = obj["score1"];
+        score2.innerHTML = obj["score2"];
+    });
+}
+
+function main() {
+    updateText();
+    setInterval(updateText, 1000);
+}
+
+let players;
+readPlayers = async () => {
+    const response = await fetch('players.json');
+    const data = await response.text();
+    const obj = JSON.parse(data);
+    players = obj["players"];
+    console.log(players);
+
+    {
+        player = player1Elements.image;
+        players.forEach(element => {
+            let img = createPlayerImage("players/" + element.player + "_L.png");
+            player.appendChild(img);
+        });
+    }
+    {
+        player = player2Elements.image;
+        players.forEach(element => {
+            let img = createPlayerImage("players/" + element.player + "_R.png");
+            player.appendChild(img);
+        });
+    }
+}
+readPlayers().then(main);
