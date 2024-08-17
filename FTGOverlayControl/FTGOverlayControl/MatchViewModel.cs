ac037b94d249295693e598eeb54e69ee2dec1c23
ud <mizunoawa.web@gmail.com>
2024-08-17 09:20:29 +0900

    追加漏れ

---
+++ FTGOverlayControl/FTGOverlayControl/MatchViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTGOverlayControl
+{
+    internal class MatchViewModel
+    {
+        private Model.Match _match;
+        private PlayerDatas _players;
+
+        public MatchViewModel(Model.Match match, PlayerDatas player) 
+        { 
+            _match = match;
+            _players = player;
+        }
+
+        public override string ToString()
+        {
+            return $"{_match.CenterText} {_players.players[_match.Player1Index].name} vs {_players.players[_match.Player2Index].name}";
+        }
+    }
+}
---
+++ FTGOverlayControl/FTGOverlayControl/Model/Match.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTGOverlayControl.Model
+{
+    internal class Match
+    {
+        public int Player1Index { get; }
+        public int Player2Index { get; }
+        public string CenterText { get; } = string.Empty;
+
+        public Match(int player1, int player2, string centerText) 
+        {
+            Player1Index = player1;
+            Player2Index = player2;
+            CenterText = centerText;
+        }
+    }
+}
