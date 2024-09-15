2ba40042c46e7b8304eadab243cc16b68a31391a
ud <mizunoawa.web@gmail.com>
2024-09-15 11:30:25 +0900

    総当たり用コントロール

---
+++ FTGOverlayControl/RoundRobinControl/MainVM.cs
@@ -0,0 +1,28 @@
+﻿using FTGOverlayControl.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoundRobinControl
+{
+    public class MainVM
+    {
+        private static string FightOrderFileName = "matches.json";
+        private ParallelMatches _matchModels;
+
+        public MainVM()
+        { 
+            _matchModels = JsonSettingIO.Read<ParallelMatches>(FightOrderFileName);
+
+            foreach(var matchModel in _matchModels.Items)
+            {
+                foreach(var match in matchModel.Items)
+                {
+                    Console.WriteLine($"{match.Player1Index} vs {match.Player2Index}");
+                }
+            }
+        }
+    }
+}
