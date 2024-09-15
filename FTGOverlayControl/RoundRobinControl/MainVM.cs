35333ab1eef1d765d5322fed5614d3b36f703823
ud <mizunoawa.web@gmail.com>
2024-09-15 14:51:09 +0900

    マッチ切替

--- FTGOverlayControl/RoundRobinControl/MainVM.cs
+++ FTGOverlayControl/RoundRobinControl/MainVM.cs
@@ -2,24 +2,61 @@
 using FTGOverlayControl.Model;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RoundRobinControl
 {
-    internal class MainVM
+    internal class MainVM : INotifyPropertyChanged
     {
         private static string PlayerFileName = "contents/players.json";
         private static string FightOrderFileName = "matches.json";
         private ParallelMatches _matchModels;
-        public IList<MatchViewModel> Matches { get; private set; }
+
+        private int _roundIndex = 0;
+        private IList<RoundViewModel> _rounds;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private RoundViewModel _matches;
+        public RoundViewModel Matches
+        {
+            get { return _matches; }
+            private set
+            {
+                _matches = value;
+                OnPropertyChanged(nameof(Matches));
+            }
+        }
+
+        public RelayCommand OnNext { get; private set; }
+        public RelayCommand OnPreview { get; private set; }
 
         public MainVM()
         { 
             var players = JsonSettingIO.Read<PlayerDatas>(PlayerFileName);
             _matchModels = JsonSettingIO.Read<ParallelMatches>(FightOrderFileName);
-            Matches = _matchModels.Items.First().Items.Select(x => new MatchViewModel(x, players)).ToList();
+            _rounds = _matchModels.Items.Select((x, index) => new RoundViewModel(index + 1, x.Items, players)).ToList();
+            Matches = _rounds.First();
+
+            OnNext = new RelayCommand(_ =>
+            {
+                _roundIndex++;
+                Matches = _rounds[_roundIndex];
+            }, _ => (_roundIndex + 1) < _rounds.Count);
+
+            OnPreview = new RelayCommand(_ =>
+            {
+                _roundIndex--;
+                Matches = _rounds[_roundIndex];
+            }, _ => _roundIndex > 0 );
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
---
+++ FTGOverlayControl/RoundRobinControl/RoundViewModel.cs
@@ -0,0 +1,24 @@
+﻿using FTGOverlayControl;
+using FTGOverlayControl.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoundRobinControl
+{
+    internal class RoundViewModel
+    {
+        private int _round;
+        public string Title => $"第{_round}試合";
+
+        public IList<MatchViewModel> MatchViewModels { get; }
+
+        public RoundViewModel(int round, IList<Match> matchViewModels, PlayerDatas players)
+        {
+            _round = round;
+            MatchViewModels = matchViewModels.Select(x => new MatchViewModel(x, players)).ToList();
+        }
+    }
+}
