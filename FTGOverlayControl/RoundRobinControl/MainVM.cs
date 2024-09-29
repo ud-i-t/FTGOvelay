0afac9219d1d2b08b5addebcbdf7f2773c1f6c22
ud <mizunoawa.web@gmail.com>
2024-09-29 10:14:48 +0900

    アナウンス用テキストを追加

--- FTGOverlayControl/RoundRobinControl/MainVM.cs
+++ FTGOverlayControl/RoundRobinControl/MainVM.cs
@@ -6,6 +6,7 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,6 +32,11 @@ namespace RoundRobinControl
             {
                 _matches = value;
                 OnPropertyChanged(nameof(Matches));
+
+                Announce = "@everyone\n" +
+                    $"第{_roundIndex + 1}試合は以下の組み合わせです\n" + 
+                    _matches.MatchViewModels.Aggregate("", (acc, x) => (acc + $"1P側: {x.Player1Name} VS 2P側: {x.Player2Name}\n")) +
+                    "準備ができ次第対戦を始めてください！";
             }
         }
 
@@ -40,6 +46,17 @@ namespace RoundRobinControl
         public RelayCommand OnPreview { get; private set; }
         public RelayCommand OnSave { get; private set; }
         public RelayCommand OnExportMatchList { get; private set; }
+
+        public string _announce = "";
+        public string Announce 
+        {
+            get{ return _announce; }
+            private set
+            {
+                _announce = value;
+                OnPropertyChanged(nameof(Announce));
+            }
+        }
 
         public MainVM()
         {
@@ -65,7 +82,7 @@ namespace RoundRobinControl
                 Save();
             }, _ => true);
 
-            OnExportMatchList = new RelayCommand(_ => ExportMatchList());
+            OnExportMatchList = new RelayCommand(_ => ExportMatchList(CalcScore()));
         }
 
         protected virtual void OnPropertyChanged(string propertyName)
@@ -73,28 +90,8 @@ namespace RoundRobinControl
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        private void Save()
+        private PlayersScore CalcScore()
         {
-            // 全試合結果
-            var results = new RoundRobinMatchResults();
-            results.Results = _rounds.SelectMany(x => x.MatchViewModels.Select(match => match.ToResult())).ToArray();
-            int lastmatch = results.Results.Count(x => x.Winner != -1);
-
-            int matchCountInRound = _rounds.First().MatchViewModels.Count();
-            if (lastmatch > 0)
-            {
-                foreach (var match in results.Results.Skip(lastmatch - matchCountInRound).Take(matchCountInRound))
-                {
-                    match.IsLast = true;
-                }
-            }
-            foreach (var match in results.Results.Skip(lastmatch).Take(matchCountInRound))
-            {
-                match.IsNext = true;
-            }
-
-            JsonSettingIO.ToJson("results.json", results);
-
             // プレイヤー成績
             var playerResults = new PlayersScore();
 
@@ -126,8 +123,35 @@ namespace RoundRobinControl
                     rank += group.Count();
                 }
 
-                JsonSettingIO.ToJson("playersScore.json", playerResults);
             }
+
+            return playerResults;
+        }
+
+        private void Save()
+        {
+            // 全試合結果
+            var results = new RoundRobinMatchResults();
+            results.Results = _rounds.SelectMany(x => x.MatchViewModels.Select(match => match.ToResult())).ToArray();
+            int lastmatch = results.Results.Count(x => x.Winner != -1);
+
+            int matchCountInRound = _rounds.First().MatchViewModels.Count();
+            if (lastmatch > 0)
+            {
+                foreach (var match in results.Results.Skip(lastmatch - matchCountInRound).Take(matchCountInRound))
+                {
+                    match.IsLast = true;
+                }
+            }
+            foreach (var match in results.Results.Skip(lastmatch).Take(matchCountInRound))
+            {
+                match.IsNext = true;
+            }
+
+            JsonSettingIO.ToJson("results.json", results);
+
+            var playerResults = CalcScore();
+            JsonSettingIO.ToJson("playersScore.json", playerResults);
 
             // サマリー
             {
@@ -139,16 +163,17 @@ namespace RoundRobinControl
             }
         }
 
-        private void ExportMatchList()
+        private void ExportMatchList(PlayersScore score)
         {
             var round = _rounds[_roundIndex];
-            string matchListString = round.MatchViewModels.Aggregate("", (string s, MatchViewModel x) => s + x.ToMatchListRow(_players.players));
+            string matchListString = round.MatchViewModels.Aggregate("", (string s, MatchViewModel x) => s + x.ToMatchListRow(_players.players, score));
             File.WriteAllText("contents/players.txt", matchListString);
 
             var app = new ProcessStartInfo();
             app.FileName = "python";
             app.Arguments = "makeMatchList.py";
             app.WorkingDirectory = "contents";
+            app.CreateNoWindow = false;
 
             Process.Start(app);
         }
@@ -156,10 +181,12 @@ namespace RoundRobinControl
 
     static class MatchViewModelExtentions
     {
-        public static string ToMatchListRow(this MatchViewModel match, PlayerData[] players)
+        public static string ToMatchListRow(this MatchViewModel match, PlayerData[] players, PlayersScore score)
         {
-            var p1 = players[match.Player1Index];
-            var p2 = players[match.Player2Index];
+            int p1ID = match.Player1Index;
+            int p2ID = match.Player2Index;
+            var p1 = players[p1ID];
+            var p2 = players[p2ID];
             string p1Win = "";
             string p2Win = "";
             if (match.IsDone())
@@ -167,7 +194,9 @@ namespace RoundRobinControl
                 p1Win = match.IsWinner(0) ? "W" : "L";
                 p2Win = match.IsWinner(1) ? "L" : "W";
             }
-            return $"{p1Win},{p1.name},{p1.team},{p1.character},{p1.rank}\n{p2Win},{p2.name},{p2.team},{p2.character},{p2.rank}\n";
+            string retval = $"{p1Win},{p1.name},{p1.team},{p1.character},{p1.rank},{score.Scores[p1ID].Rank},{score.Scores[p1ID].Score}\n";
+            retval += $"{p2Win},{p2.name},{p2.team},{p2.character},{p2.rank},{score.Scores[p2ID].Rank},{score.Scores[p2ID].Score}\n";
+            return retval;
         }
     }
 }
