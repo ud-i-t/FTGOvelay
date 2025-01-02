ff72bd33a845d3d3c877f5cc3334f1d68017bc4e
ud <mizunoawa.web@gmail.com>
2025-01-02 12:58:19 +0900

    大会ルール設定

--- FTGOverlayControl/FTGOverlayControl/RuleEditVm.cs
+++ FTGOverlayControl/FTGOverlayControl/RuleEditVm.cs
@@ -1,15 +1,18 @@
 ﻿using FTGOverlayControl.Model;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FTGOverlayControl
 {
-    class RuleEditVm
+    class RuleEditVm : INotifyPropertyChanged
     {
-        public List<Position> Positions { get; } = new List<Position>
+        public ObservableCollection<Position> Positions { get; } = new ObservableCollection<Position>();
+        private List<Position> _positions { get; } = new List<Position>
         {
             new Position(){ Index = "#1", Name = "先鋒" },
             new Position(){ Index = "#2", Name = "次鋒" },
@@ -17,5 +20,46 @@ namespace FTGOverlayControl
             new Position(){ Index = "#4", Name = "副将" },
             new Position(){ Index = "#5", Name = "大将" },
         };
+        public RelayCommand ChangePlayerCount { get; private set; }
+
+        private int _playerCount = 5;
+        public int PlayerCount 
+        { 
+            get
+            {
+                return _playerCount;
+            }
+            set
+            {
+                _playerCount = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PlayerCount)));
+            }
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public RuleEditVm()
+        {
+            ChangePlayerCount = new RelayCommand(_ =>
+            {
+                if (PlayerCount > _positions.Count)
+                {
+                    AddPlayer();
+                }
+
+                Positions.Clear();
+                foreach (var p in _positions.Take(PlayerCount))
+                {
+                    Positions.Add(p);
+                }
+            }, _ => true);
+
+            ChangePlayerCount.Execute(null);
+        }
+
+        private void AddPlayer()
+        {
+            _positions.Add(new Position() { Index = $"#{_positions.Count + 1}" });
+        }
     }
 }
